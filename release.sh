895a4b784ef6ed541e0eb98f365762fac1956b7b
JavaCommons Technologies <javacommons@gmail.com>
2024-01-26 12:50:02 +0900

    Release v0.1.0

--- release.sh
+++ release.sh
@@ -9,5 +9,6 @@ git tag -a v$version -mv$version
 git push origin v$version
 git push
 git remote -v
+gh auth login --with-token < ~/settings/github-all-tokne.txt
 #gh release create v$version --repo lang-library/go-global --generate-notes --target main
 gh release create v$version --generate-notes --target main
